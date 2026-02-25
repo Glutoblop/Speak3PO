df4303dceee66bbdff6962016fbe6f8cb767569b
Jordan Nye <jordannye@hotmail.com>
2026-02-25 09:58:34 +0000

    Updated script to pull from public repo

--- Scripts/pull_git_docker_up.bat
+++ Scripts/pull_git_docker_up.bat
@@ -5,7 +5,7 @@ RMDIR /S /Q build
 :: pull down my git repo, inside that repo is a script that will build itself, create docker image and run image in a local docker container
 :: it assumes docker is running on the environment you are in.
 :: TODO - Should just put the release artifacts not the entire repo for this
-git clone --branch main https://gitlab.com/nyetools/speak3po.git build
+git clone --branch main https://github.com/Glutoblop/Speak3PO.git build
 
 :: Alongside this script is a dependency for my bot, a config file the bot needs to grab when it runs, copy that in. 
 echo f | xcopy ".\config.json" ".\build\config.json"
